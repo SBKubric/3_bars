696955d746a99f2983f9c37a17ba248c4f5d3f51
SBKubric <sbkubric@gmail.com>
2016-10-24 23:02:13 +0300

    Fixed the errors

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 3_bars
+
+It is a script, that shows the biggest bar, the smallest bar and the nearest bar in Moscow.
+
+
+## The data.
+
+I used a data dump from data.mos.ru
+Here is the [link] (http://data.mos.ru/opendata/7710881420-bary)
+Just download it and run the script
+
+
+## The file location
+If you placed the JSON file right into the script directory, just type in the filename.
+However, if you want to store data separately from script you will need to type in full path to the JSON file.
+
+
+## The nearest bar
+
+The distance between you and a bar is calculated by the [Havershine formula] (http://www.movable-type.co.uk/scripts/latlong.html)
+So you will need to type in your latitude and longitude to locate the nearest place.
--- bars.py
+++ bars.py
@@ -1,70 +1,80 @@
-import json, math
+import json
+import math
+import os
 
 
-def load_data(filepath='Бары.json'):
-    with open(filepath) as data_file:
-        data = json.load(data_file)
-    return data
+def distance(latitude1, latitude2, longitude1, longitude2):
+    """
+    The distance is calculated by the Havershine formula.
+    :param latitude1: the latitude of location1
+    :param latitude2: the latitude of location2
+    :param longitude1: the longitude of location1
+    :param longitude2: the longitude of location2
+    :return: the distance in km
+    """
+    earth_radius = 6371
+
+    latitude1 = math.radians(latitude1)
+    latitude2 = math.radians(latitude2)
+    delta_latitude = math.radians(latitude2 - latitude1)
+    delta_longitude = math.radians(longitude2 - longitude1)
+
+    a = math.sin(delta_latitude / 2) ** 2 + math.cos(latitude1) * math.cos(latitude2) * (
+    math.sin(delta_longitude / 2) ** 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return earth_radius * c
 
 
-def get_biggest_bar(data):
-    max = (data[0]['Cells']['Name'], data[0]['Cells']['SeatsCount'])
-    for json_object in data:
-        if max[1] < json_object['Cells']['SeatsCount']:
-            max = (json_object['Cells']['Name'], json_object['Cells']['SeatsCount'])
-    print('The largest place is "%s"' % max[0])
+def load_json(json_path):
+    if not os.path.exists(json_path):
+        return None
+    with open(json_path) as data_file:
+        return json.load(data_file)
 
 
-def get_smallest_bar(data):
-    min = (data[0]['Cells']['Name'], data[0]['Cells']['SeatsCount'])
-    for json_object in data:
-        if min[1] > json_object['Cells']['SeatsCount']:
-            min = (json_object['Cells']['Name'], json_object['Cells']['SeatsCount'])
-    print('The smallest place is "%s"' % min[0])
+def get_biggest_bar(json_bars):
+    bars = {(json_bar['Cells']['SeatsCount'], json_bar['Cells']['Name']) for json_bar in json_bars}
+    biggest_bar = max(bars)
+    return biggest_bar[1]
 
 
-def get_closest_bar(data, longitude, latitude):
-    def distance(f1, f2, l1, l2):
-        R = 6.371
+def get_smallest_bar(json_bars):
+    bars = {(json_bar['Cells']['SeatsCount'], json_bar['Cells']['Name']) for json_bar in json_bars}
+    smallest_bar = min(bars)
+    return smallest_bar[1]
 
-        f1 = math.radians(f1)
-        f2 = math.radians(f2)
-        df = math.radians(f2 - f1)
-        dl = math.radians(l2 - l1)
 
-        a = math.sin(df/2)**2 + math.cos(f1)*math.cos(f2)*(math.sin(dl/2)**2)
-        c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
-        return R*c
+def get_closest_bar(json_bars, user_longitude, user_latitude):
+    bars = {(distance(
+                       user_latitude,
+                       float(json_bar['Cells']['geoData']['coordinates'][1]),
+                       user_longitude,
+                       float(json_bar['Cells']['geoData']['coordinates'][0]),
+                   ),
+             json_bar['Cells']['Name']) for json_bar in json_bars
+            }
+    closest_bar = min(bars)
+    return closest_bar[1]
 
-    nearest = {'name': data[0]['Cells']['Name'],
-           'dist': distance(
-               latitude,
-               float(data[0]['Cells']['geoData']['coordinates'][1]),
-               longitude,
-               float(data[0]['Cells']['geoData']['coordinates'][0]),
-           ),
-           }
-
-    for json_object in data:
-        if nearest['dist'] > distance(latitude, float(json_object['Cells']['geoData']['coordinates'][1]), longitude, float(json_object['Cells']['geoData']['coordinates'][0])):
-            nearest['name'] = json_object['Cells']['Name']
-            nearest['dist'] = distance(latitude, float(json_object['Cells']['geoData']['coordinates'][1]), longitude, float(json_object['Cells']['geoData']['coordinates'][0]))
-
-    print('The nearest place is "%s"' % nearest['name'])
 
 if __name__ == '__main__':
     print('Enter the path to the file:')
-    i = input()
-    if i == '':
-        data = load_data()
-    else:
-        data = load_data(i)
+    json_path = input()
+    json_bars = load_json(json_path)
 
-    get_biggest_bar(data)
-    get_smallest_bar(data)
+    while json_bars is None:
+        print('Sorry, but it looks like there is a mistake in the path to the file.'
+              '\nPlease, enter the valid path:', end=' ')
+        json_path = input()
+        json_bars = load_json(json_path)
 
-    print('Enter the latitude:')
-    latitude = float(input())
-    print('Enter the longitude:')
-    longitude = float(input())
-    get_closest_bar(data, longitude, latitude)
+    print('The biggest bar is %s' % get_biggest_bar(json_bars))
+    print('The smallest bar is %s' % get_smallest_bar(json_bars))
+
+    print('Enter your latitude:')
+    user_latitude = float(input())
+
+    print('Enter your longitude:')
+    user_longitude = float(input())
+
+    print('The nearest place is "%s"' % get_closest_bar(json_bars, user_longitude, user_latitude))
